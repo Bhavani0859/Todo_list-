740c194d4902a2afbb5ef5e00234c97e419f8aca
Bhavani0859 <90919367+Bhavani0859@users.noreply.github.com>
2022-10-27 10:52:08 +0530

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -2,15 +2,14 @@ let todoele = document.getElementById("taskinfo")
 let tasklist = document.getElementById("tasklist")
 let addBtn =document.getElementById("add")
 let value1=[]
-localStorage.getItem("element")
 addBtn.onclick =function(){
     if(todoele.value.length==0){
         alert("Enter details of task")
     }
     else{
 	/*console.log(tasklist.innerHTML)*/
-	if(tasklist.innerHTML==""){
-		console.log("TRTYUI")}
+	/*if(tasklist.innerHTML==""){
+		console.log("BhaVani")}*/
         tasklist.innerHTML = tasklist.innerHTML+`    
         <div class="task"> 
             <span id="work">${todoele.value}</span>
@@ -19,24 +18,26 @@ addBtn.onclick =function(){
 	let alltasks = document.querySelectorAll('.del')
 	value1.push(todoele.value)
 	
-         localStorage.setItem("element",value1)
+         //localStorage.setItem("element",value1)
          for(i=0;i<alltasks.length;i++){
                 alltasks[i].onclick = function(){
                     console.log(this)
                     console.log(this.parentNode)
                 	  this.parentNode.remove()
-	           localStorage.removeItem(localStorage[i])
+	           //localStorage.removeItem(localStorage[i])
                 }
           }
          todoele.value=""
+        }
+    console.log(tasklist.innerHTML)
 	let clear=document.getElementById("clearlist")
-	
-    	}}
-	clearlist.onclick=function(){
+	clear.onclick=function(){
+        console.log("in to")
 		if(tasklist.innerHTML==""){
 			alert("List is already empty")}
 		else{
 			tasklist.innerHTML=''}
 		}
+}
 
 
